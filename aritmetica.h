c261325676897a20719bba26ecb5b56c67a23a3f
joamessina <40438015+joamessina@users.noreply.github.com>
2021-04-16 22:22:52 -0300

    TP calculadora

---
+++ aritmetica.h
@@ -0,0 +1,21 @@
+/*
+ Joaquin Gabriel Messina
+ */
+#include<stdio.h>
+#ifndef ARITMETICA_H_
+#define ARITMETICA_H_
+
+	float suma(float numero_1, float numero_2);
+
+	float resta(float numero_1, float numero_2);
+
+	float multiplicacion(float numero_1, float numero_2);
+
+	float division(float numero_1, float numero_2);
+
+	int factorial(float numero_1);
+
+	float menu();
+
+#endif /* ARITMETICA_H_ */
+
